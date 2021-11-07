d2bb2d16e5ce286c5d1f66f78cd242623ab5d408
HassanTAli <hassantarekha@gmail.com>
2021-11-07 15:51:09 +0200

    add static_root

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[os.path.join(BASE_DIR,'main_static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS =[os.path.join(BASE_DIR,'staticfiles')]
+STATIC_ROOT =  '/home/hassantali/Django/static_root'
 
 # \home\hassantali\Django\static_root
