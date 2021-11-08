5b85f6afd383e5ec1a8d1620e0c34cd84ba859a1
HassanTAli <hassantarekha@gmail.com>
2021-11-08 14:45:06 +0200

    add movies url

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('todo.urls')),
+    path('todo/', include('todo.urls')),
+    path('movie/', include('movies.urls')),
 ]
