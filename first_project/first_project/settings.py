64e29082959b1f50b355e9c817feba11ff5b909d
Hassan Tarek <55896820+HassanTAli@users.noreply.github.com>
2021-11-09 19:49:57 +0200

    remove password

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -139,7 +139,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'hassan.tarek.hassan.ali@gmail.com'
-EMAIL_HOST_PASSWORD = 'Sh_ha172733'
+EMAIL_HOST_PASSWORD = ''
 
 LOGIN_REDIRECT_URL = '/movie/movie_index'
 LOGOUT_REDIRECT_URL = '/movie/movie_index'
